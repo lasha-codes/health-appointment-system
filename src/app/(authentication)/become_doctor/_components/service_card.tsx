27eaaff0cbff6c5ba698191a50e7ee49b0e39623
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-07-16 20:46:57 +0400

    service_card design is done

--- src/app/(authentication)/become_doctor/_components/service_card.tsx
+++ src/app/(authentication)/become_doctor/_components/service_card.tsx
@@ -1,7 +1,25 @@
 import { service_type } from '@/data/services/services'
+import Image from 'next/image'
 
 const ServiceCard = ({ id, title, image, summary }: service_type) => {
-  return <div className='bg-white shadow-sm'></div>
+  return (
+    <div className='bg-white cursor-pointer shadow-sm flex items-start overflow-hidden gap-10 w-[530px] h-[190px] rounded-xl'>
+      <div className='min-w-[50%] max-w-[50%] relative h-full'>
+        <Image
+          src={image}
+          alt={title + '' + 'Image'}
+          fill
+          className='object-cover'
+        />
+      </div>
+      <div className='flex flex-col items-start gap-2 py-2'>
+        <h3 className='capitalize text-[17px] font-medium text-main_green'>
+          {title}
+        </h3>
+        <p className='text-[12px] text-gray-500'>{summary}</p>
+      </div>
+    </div>
+  )
 }
 
 export default ServiceCard
--- src/app/(authentication)/become_doctor/page.tsx
+++ src/app/(authentication)/become_doctor/page.tsx
@@ -1,23 +1,32 @@
 import DoctorForm from './_components/doctor_form'
+import ServiceCard from './_components/service_card'
+import { service_type, services } from '@/data/services/services'
 
 const BecomeDoctorPage = () => {
   return (
     <main className='mt-10'>
-      <div className='w-full flex items-start justify-between gap-20 max-lg:flex-col'>
-        <div className='w-full'>
-          <div className='flex flex-col items-start gap-5'>
-            <h2 className='text-2xl font-medium text-black'>
-              How to become doctor at Healthcare.
-            </h2>
-            <p className='text-[15px] max-w-[500px] text-gray-500'>
-              Hello dear user, if u are trying to become doctor at Healthcare
-              company, we would first like you to fill out the form below this,
-              after that we will receive your information hiring manager will
-              decide u will either be acepted or not, if something goes wrong we
-              can always contact you via your email, or phone number.
-            </p>
+      <div className='flex items-start justify-between gap-5'>
+        <div className='w-full flex items-start justify-between gap-20 max-lg:flex-col'>
+          <div className='w-full'>
+            <div className='flex flex-col items-start gap-5'>
+              <h2 className='text-2xl font-medium text-black'>
+                How to become doctor at Healthcare.
+              </h2>
+              <p className='text-[15px] max-w-[500px] text-gray-500'>
+                Hello dear user, if u are trying to become doctor at Healthcare
+                company, we would first like you to fill out the form below
+                this, after that we will receive your information hiring manager
+                will decide u will either be acepted or not, if something goes
+                wrong we can always contact you via your email, or phone number.
+              </p>
+            </div>
+            <DoctorForm />
           </div>
-          <DoctorForm />
+        </div>
+        <div className='flex items-center gap-5 flex-wrap'>
+          {services.map((service: service_type) => {
+            return <ServiceCard {...service} key={service.id} />
+          })}
         </div>
       </div>
     </main>
